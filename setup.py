5e961219e51f90d108672f201ba41054bd96d26c
SCiarella <simoneciarella@gmail.com>
2024-01-26 09:46:31 +0100

    Update git actions

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     url='https://github.com/MALES-project/SpeckleCn2Profiler',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
+    python_requires='>=3.10.0',
     packages=find_packages(),
     install_requires=[
         'numpy',
@@ -27,9 +28,12 @@ setup(
         'laser communication'
     ],
     classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Education',
+        'Development Status :: 2 - Pre-alpha',
+        'Intended Audience :: Science/Research',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Operating System :: Unix',
+        'Topic :: Scientific/Engineering',
     ],
 )
