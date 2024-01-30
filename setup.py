81063abf51784b9d06e640480348af7c420fc05a
SCiarella <simoneciarella@gmail.com>
2024-01-30 09:58:56 +0100

    Update action

--- setup.py
+++ setup.py
@@ -12,11 +12,11 @@ setup(
     url='https://github.com/MALES-project/SpeckleCn2Profiler',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
-    python_requires='>=3.10.0',
+    python_requires='>=3.9.0',
     packages=find_packages(),
     install_requires=[
         'numpy', 'pandas', 'matplotlib', 'torch', 'torchvision', 'scipy',
-        'PyYAML', 'python-dev-tools', 'escnn'
+        'PyYAML', 'escnn'
     ],
     keywords=[
         'python', 'optical satelites', 'machine learning', 'turbolence',
@@ -25,7 +25,7 @@ setup(
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Science/Research',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Operating System :: Unix',
