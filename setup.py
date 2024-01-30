b9804c12fb1d902a57c310af872df903b5905d51
SCiarella <simoneciarella@gmail.com>
2024-01-30 12:23:04 +0100

    Update action

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         'numpy', 'pandas', 'matplotlib', 'torch', 'torchvision', 'scipy',
-        'PyYAML', 'py3nj', 'escnn'
+        'PyYAML', 'Cython', 'escnn'
     ],
     keywords=[
         'python', 'optical satelites', 'machine learning', 'turbolence',
