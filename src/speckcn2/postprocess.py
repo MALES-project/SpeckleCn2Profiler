367078e690e93b2bd60a8910e9d98ba937af96eb
SCiarella <simoneciarella@gmail.com>
2024-04-04 16:54:46 +0200

    Update Citations

--- src/speckcn2/postprocess.py
+++ src/speckcn2/postprocess.py
@@ -59,6 +59,10 @@ def tags_distribution(conf: dict,
     print(f'Prediction mean: {predic_tags.mean()}')
     print(f'Prediction std: {predic_tags.std()}')
 
+    # Keep track of J=sum(tags) for each sample
+    J_pred = np.zeros(train_tags.shape[0])
+    J_true = np.zeros(train_tags.shape[0])
+
     # Plot the distribution of each tag element
     fig, axs = plt.subplots(2, 4, figsize=(20, 10))
     for i in range(train_tags.shape[1]):
@@ -79,6 +83,12 @@ def tags_distribution(conf: dict,
                                     density=True,
                                     alpha=0.5,
                                     label='Training data')
+            print(recovered_tag_model.shape)
+            print(recovered_tag_true.shape)
+            print(J_pred.shape)
+            print(J_true.shape)
+            J_pred += recovered_tag_model
+            J_true += recovered_tag_true
         else:
             axs[i // 4, i % 4].hist(predic_tags[:, i],
                                     bins=20,
@@ -101,3 +111,25 @@ def tags_distribution(conf: dict,
         plt.savefig(
             f'{data_directory}/result_plots/{model_name}_tags_unscaled.png')
     plt.close()
+
+    if rescale and recover_tag is not None:
+        # Also plot the distribution of the sum of the tags
+        fig, axs = plt.subplots(1, 1, figsize=(6, 6))
+        axs.hist(J_pred,
+                 bins=20,
+                 color='tab:red',
+                 density=True,
+                 alpha=0.5,
+                 label='Model prediction')
+        axs.hist(J_true,
+                 bins=20,
+                 color='tab:blue',
+                 density=True,
+                 alpha=0.5,
+                 label='Training data')
+        axs.set_title('Sum of tags')
+        axs.legend()
+        plt.tight_layout()
+        plt.savefig(f'{data_directory}/result_plots/{model_name}_sumJ.png')
+
+        plt.close()
