3d6b6218ad90b826515aa71600aef8f620711512
SCiarella <simoneciarella@gmail.com>
2024-05-29 22:12:38 +0200

    Fix Spider
    Implement signal-to-noise-ratio
    
    Add histos of parameters

--- src/speckcn2/mlops.py
+++ src/speckcn2/mlops.py
@@ -54,7 +54,8 @@ def train(model: nn.Module, last_model_state: int, conf: dict, train_set: list,
     # Setup the EnsembleModel wrapper
     ensemble = EnsembleModel(conf['preproc'].get('ensemble', 1), device,
                              conf['preproc'].get('ensemble_unif', False),
-                             conf['model'].get('add_noise', False))
+                             conf['model'].get('noise',
+                                               0), conf['preproc']['resize'])
 
     print(f'Training the model from epoch {last_model_state} to {final_epoch}')
     average_loss = 0.0
@@ -168,7 +169,8 @@ def score(
     # Setup the EnsembleModel wrapper
     ensemble = EnsembleModel(conf['preproc'].get('ensemble', 1), device,
                              conf['preproc'].get('ensemble_unif', False),
-                             conf['model'].get('add_noise', False))
+                             conf['model'].get('noise',
+                                               0), conf['preproc']['resize'])
 
     # For scoring the model, I enforce to use with the same weights:
     # 1. MAE on screen tags
