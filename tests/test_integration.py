b1c96f76c9f34c0b9a0f55a2a46fb7cf3efe5dc9
SCiarella <simoneciarella@gmail.com>
2024-10-01 17:22:04 +0200

    add cleanup before tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
+import glob
 import os
 import runpy
+import shutil
 from pathlib import Path
 
 import pytest
@@ -9,13 +11,33 @@ import torch
 
 import speckcn2 as sp2
 
-CONF_YAML = ('tests/test_data/conf_resnet.yaml',
-             'tests/test_data/conf_scnn.yaml')
+CONF_YAML = (('tests/test_data/conf_resnet.yaml', 'resnet'),
+             ('tests/test_data/conf_scnn.yaml', 'scnn'))
 
 
-@pytest.mark.parametrize('conf', CONF_YAML)
-@pytest.mark.dependency(name='test_example')
-def test_example(conf):
+# Function to remove files or directories matching a pattern
+def remove_files(pattern, is_dir=False):
+    for path in glob.glob(pattern):
+        if is_dir:
+            shutil.rmtree(path)
+        else:
+            os.remove(path)
+
+
+@pytest.mark.parametrize(('conf', 'model_type'), CONF_YAML)
+@pytest.mark.dependency(name='test_train_and_score')
+def test_train_and_score(conf, model_type):
+    basefolder = 'tests/test_data/speckles/'
+    patterns = {
+        'model_folders': f'{basefolder}Model_test_{model_type}*',
+        'datasets': f'{basefolder}all_*{model_type}*pt',
+        'train_test_splits':
+        f'{basefolder}t*_set_Model_test_{model_type}*pickle'
+    }
+    remove_files(patterns['model_folders'], is_dir=True)
+    remove_files(patterns['datasets'])
+    remove_files(patterns['train_test_splits'])
+
     train = runpy.run_path('examples/example_train.py')
     train['main'](conf)
 
@@ -23,9 +45,9 @@ def test_example(conf):
     post['main'](conf)
 
 
-@pytest.mark.parametrize('conf', CONF_YAML)
-@pytest.mark.dependency(depends=['test_example'])
-def test_example_figures(conf, image_diff):
+@pytest.mark.parametrize(('conf', 'model_type'), CONF_YAML)
+@pytest.mark.dependency(depends=['test_train_and_score'])
+def test_figures(conf, model_type, image_diff):
     basefolder = 'tests/test_data/speckles/expected_results/'
     os.walk(basefolder)
     for single_folder in os.walk(basefolder):
@@ -40,9 +62,9 @@ def test_example_figures(conf, image_diff):
                 image_diff(expected, test_img)
 
 
-@pytest.mark.parametrize('conf', CONF_YAML)
-@pytest.mark.dependency(depends=['test_example'])
-def test_example_models(conf):
+@pytest.mark.parametrize(('conf', 'model_type'), CONF_YAML)
+@pytest.mark.dependency(depends=['test_train_and_score'])
+def test_weights(conf, model_type):
     config = sp2.load_config(conf)
     datadirectory = config['speckle']['datadirectory']
     test_model, _ = sp2.setup_model(config)
